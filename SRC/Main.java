cd38197417b7d8c31e53ffb1ce30569d6de741b7
erick <erickrv@ciencias.unam.mx>
2026-09-13 22:25:14 -0600

    Documentación/comentarios y archivos de prueba que si sirve gg no lo lean cai en la locura

--- SRC/Main.java
+++ SRC/Main.java
@@ -10,63 +10,65 @@ import java.io.IOException;
  **/
 public class Main {
 
-      /**
-     * Método auxiliar que verifica que la entrada del usuario sea un número válido.
-     * @param mensaje Mensaje de advertencia.
-     * @return Entero valido.
-     */
-      private static long entrada(String mensaje) {
-        while (true) {
-          try {
-            System.out.print(mensaje);
-            return scanner.nextLong();
-          } catch (InputMismatchException e) {
-            System.out.println("Ingresa un numero valido.");
-            scanner.nextLong();
-          }
+    /**
+ * Método auxiliar para leer una entrada de texto.
+ * @param mensaje Mensaje para el usuario.
+ * @return Entrada de texto válida.
+ */
+  private static String entradaTexto(String mensaje) {
+    System.out.print(mensaje);
+    return scanner.nextLine();
+  }   
+
+  /**
+  * Método auxiliar que verifica que la entrada del usuario sea un número válido.
+  * @param mensaje Mensaje de advertencia.
+  * @return Entero valido.
+  */
+  private static long entrada(String mensaje) {
+    while (true) {
+      try {
+        System.out.print(mensaje);
+        return scanner.nextLong();
+      } catch (InputMismatchException e) {
+          System.out.println("Ingresa un numero valido.");
+          scanner.nextLong();
         }
       }
+    }
     
   // Declaramos el scanner para la entraada del usuario.
   private static final Scanner scanner = new Scanner(System.in);
 
-    /**
-     * Metodo main donde se despliega el menu
-     * @param args Argumentos de la línea de comandos.
-     */
-    public static void main(String[] args) {
-      boolean salir = false;
-      
-      // Bucle hasta que el usuario decida cerrar la app
-      while (!salir) {
-
-        // Manejo de excepciones para entradas invalidas
-        try {
-          mostrarMenuP();
-          long opcionLong = entrada("Selecciona una opción: ");
-          int opcion = (int) opcionLong;
-
-          switch (opcion) {
-            //Llama al metodo entidad para hacer operaciones 
-            //con el parametro "Sucursales"
-            case 1:
-              gestionarSucursales();
-              break;
-            case 2:
-            //Llama al metodo entidad para hacer operaciones 
-            //con el parametro "Premios"
-              gestionarPremios();
-              break;
-            case 3:
-              //Llama al metodo entidad para hacer operaciones 
-              //con el parametro "Clientes"
-              gestionarClientes();
-              break;
-            case 4:
-              // Cierra la app, que triste, el usuario ya no la va a usar chale ni modo
-              System.out.println("\nSaliendo de la app ):");
-              salir = true;
-              break;
+  /**
+  * Metodo main donde se despliega el menu
+  * @param args Argumentos de la línea de comandos.
+  */
+ public static void main(String[] args) {
+    boolean salir = false;
+    // Bucle hasta que el usuario decida cerrar la app
+    while (!salir) {
+      // Manejo de excepciones para entradas invalidas
+      try {
+        mostrarMenuP();
+        long opcionLong = entrada("Selecciona una opción: ");
+        int opcion = (int) opcionLong;
+        //Casos que puede eligir el usuario
+        switch (opcion) {
+          case 1:
+            gestionarSucursales();
+            break;
+          case 2:
+            gestionarPremios();
+            break;
+          case 3:
+            gestionarClientes();
+            break;
+          case 4:
+            // Cierra la app, que triste, el usuario ya no la va a usar chale ni modo
+            System.out.println("\nSaliendo de la app ):");
+            salir = true;
+            break;
               //Cualquier entrada invalida arroja una advertencia
             default:
               System.out.println("\nIngresa un número valido (entre 1 y 4.");
@@ -116,7 +118,6 @@ public class Main {
             case 2:
               long id = entrada("Ingresa llave de Sucursal: ");
               System.out.println("\nConsultando Sucursal con llave: " + id);
-              // Método para buscar en el CSV
               break;
             case 3:
               long idEdit = entrada("Ingresa la llave de Sucursal: ");
@@ -143,6 +144,7 @@ public class Main {
 
     /**
      * Metodo para gestionar Premios.
+     * Se puede agregar, consultar, editar y eliminar.
      */
     private static void gestionarPremios() {
       boolean volvermenup = false;
@@ -178,6 +180,7 @@ public class Main {
               // Método para eliminar en el CSV
               break;
             case 5:
+              //fuga
               volvermenup = true;
               break;
             default:
@@ -190,158 +193,157 @@ public class Main {
       }
     }
 
-    /**
-     * Metodo para gestionar Clientes.
-     */
-    private static void gestionarClientes() {
-      boolean volvermenup = false;
+  /**
+  * Metodo para gestionar Clientes.
+  * Se puede agregar, consultar, editar y eliminar.
+  */
+  private static void gestionarClientes() {
+    boolean volvermenup = false;
 
-      while (!volvermenup) {
-        try {
-          System.out.println("\nClientes");
-          System.out.println("1. Agregar Cliente");
-          System.out.println("2. Consultar por Llave de Cliente");
-          System.out.println("3. Editar Cliente");
-          System.out.println("4. Eliminar Cliente");
-          System.out.println("5. Volver al menú principal");
+    while (!volvermenup) {
+      try {
+        System.out.println("\nClientes");
+        System.out.println("1. Agregar Cliente");
+        System.out.println("2. Consultar por Llave de Cliente");
+        System.out.println("3. Editar Cliente");
+        System.out.println("4. Eliminar Cliente");
+        System.out.println("5. Volver al menú principal");
 
-          long opcionLong = entrada("Selecciona una operación: ");
-          int opcion = (int) opcionLong;
+        long opcionLong = entrada("Selecciona una operación: ");
+        int opcion = (int) opcionLong;
 
-          switch (opcion) {
-            case 1:
-              agregarCliente();
-              break;
-            case 2:
-              long id = entrada("Ingresa llave de Cliente: ");
-              System.out.println("\nConsultando Cliente con llave: " + id);
-              // Método para buscar en el CSV
-              break;
-            case 3:
-              long idEdit = entrada("Ingresa la llave de Cliente: ");
-              System.out.println("\nEditando Cliente con llave: " + idEdit);
-              // Método para editar en el CSV
-              break;
-            case 4:
-              long idElim = entrada("Ingresa la llave de Cliente a eliminar: ");
-              System.out.println("\nEliminando Cliente con llave: " + idElim);
-              // Método para eliminar en el CSV
-              break;
-            case 5:
-              volvermenup = true;
-              break;
+        switch (opcion) {
+          case 1:
+            agregarCliente();
+            break;
+          case 2:
+            long id = entrada("Ingresa llave de Cliente: ");
+            System.out.println("\nConsultando Cliente con llave: " + id);
+            // Método para buscar en el CSV
+            break;
+          case 3:
+            long idEdit = entrada("Ingresa la llave de Cliente: ");
+            System.out.println("\nEditando Cliente con llave: " + idEdit);
+            // Método para editar en el CSV
+            break;
+          case 4:
+            long idElim = entrada("Ingresa la llave de Cliente a eliminar: ");
+            System.out.println("\nEliminando Cliente con llave: " + idElim);
+            // Método para eliminar en el CSV
+            break;
+          case 5:
+            //Fuga
+            volvermenup = true;
+            break;
             default:
-              System.out.println("\nOpción inválida. Elige un número entre 1 y 5.");
-          }
-        } catch (Exception e) {
-          System.out.println("\nIngresa una entrada válida: " + e.getMessage());
-          scanner.nextLine();
+            System.out.println("\nOpción inválida. Elige un número entre 1 y 5.");
         }
+        //Manejo de excepciones
+      } catch (Exception e) {
+        System.out.println("\nIngresa una entrada válida: " + e.getMessage());
+        scanner.nextLine();
       }
     }
+  }
 
-/**
-     * Método para agregar una sucursal al archivo CSV.
-     */
-private static void agregarSucursal() {
-  try {
-    System.out.println("Ingresa los siguientes datos para la sucursal:");
+  /**
+  * Método para agregar una sucursal al archivo CSV.
+  */
+  private static void agregarSucursal() {
+    try {
+      System.out.println("Ingresa los siguientes datos para la sucursal:");
 
-    long idSucursal = entrada("ID de la Sucursal (debe ser npumero): ");
-    scanner.nextLine();
-    String nombreS = entradaTexto("Nombre de la Sucursal: ");
-    String calle = entradaTexto("Calle:  ");
-    long numInte = entrada("Número interior: ");
-    scanner.nextLine();
-    long numExte = entrada("Número exterior: ");
-    scanner.nextLine();
-    String colonia = entradaTexto("Colonia: ");
-    String estado = entradaTexto("Estado: ");
-    long telefonoS = entrada("Teléfono: ");
-    scanner.nextLine();
-    String horarios = entradaTexto("Horarios: ");
+      //Pedir los datos de la sucursal al usuario y los guardar en variables
+      long idSucursal = entrada("ID de la Sucursal (debe ser npumero): ");
+      scanner.nextLine();
+      String nombreS = entradaTexto("Nombre de la Sucursal: ");
+      String calle = entradaTexto("Calle:  ");
+      long numInte = entrada("Número interior: ");
+      scanner.nextLine();
+      long numExte = entrada("Número exterior: ");
+      scanner.nextLine();
+      String colonia = entradaTexto("Colonia: ");
+      String estado = entradaTexto("Estado: ");
+      long telefonoS = entrada("Teléfono: ");
+      scanner.nextLine();
+      String horarios = entradaTexto("Horarios: ");
 
-    // Guardar en el archivo CSV
-    try (FileWriter writer = new FileWriter("sucursal.csv", true)) {
-      writer.append(idSucursal + "," + nombreS + "," + calle + "," + numInte + "," + numExte + "," + colonia + "," + estado + "," + telefonoS + "," + horarios + "\n");
-      System.out.println("Sucursal agregada exitosamente.");
-    } catch (IOException e) {
-      System.out.println("Error al escribir en el archivo CSV: " + e.getMessage());
-    }
-  } catch (Exception e) {
-    System.out.println("Error al agregar la sucursal: " + e.getMessage());
-  } 
-}
-
-/**
- * Método para agregar premios
- */
-public static void agregarPremio(){
-  try{
-    System.out.println("Ingresa los siguientes datos para el premio:");
-
-    long idPremio = entrada("ID del Premio (debe ser un número): ");
-    scanner.nextLine();
-    String nombreP = entradaTexto("Nombre del Premio: ");
-    String categoria = entradaTexto("Categoría del Premio: (Bajo, Medio o Alto): ");
-    String rangoEdad = entradaTexto("Rango de Edad (infantil 3-12, juvenil 13-17, adulto 18+): ");
-    long puntos = entrada("Puntos necesarios para canjear el premio: ");
-    scanner.nextLine();
-    long cantidadS = entrada("Cantidad de premios en sucursal: ");
-    scanner.nextLine();
-    long idSucursal = entrada("ID de la Sucursal donde se encuentra el premio: ");
-  
-    try(FileWriter writer = new FileWriter("premios.csv", true)) {
-      writer.append(idPremio + "," + nombreP + "," + categoria + "," + rangoEdad + "," + puntos + "," + cantidadS + "," + idSucursal + "\n");
-      System.out.println("Premio agregado exitosamente.");
-    } catch (IOException e) {
-      System.out.println("Error al escribir en el archivo CSV: " + e.getMessage());
+      // EScribir los datos en el archivo CSV
+      try (FileWriter writer = new FileWriter("sucursal.csv", true)) {
+        writer.append(idSucursal + "," + nombreS + "," + calle + "," + numInte + "," + numExte + "," + colonia + "," + estado + "," + telefonoS + "," + horarios + "\n");
+        System.out.println("Sucursal agregada exitosamente.");
+      } catch (IOException e) {
+        System.out.println("Error al escribir en el archivo CSV: " + e.getMessage());
+      }
+      //Manejo de excepciones
+    } catch (Exception e) {
+      System.out.println("Error al agregar la sucursal: " + e.getMessage());
     } 
-  } catch (Exception e) {
-    System.out.println("Error al agregar el premio: " + e.getMessage());
   }
-}
 
-/**
- * Método para agregar clientes
- */
-public static void agregarCliente(){
-  try{
-    System.out.println("Ingresa los siguientes datos para el cliente:");
+  /**
+   * Método para agregar premios
+   */
+  public static void agregarPremio(){
+    try{
+      System.out.println("Ingresa los siguientes datos para el premio:");
 
-    long idCliente = entrada("ID del cliente (debe ser un número): ");
-    scanner.nextLine();
-    String nombreC = entradaTexto("Nombre del cliente: ");
-    String apellidoP = entradaTexto("Apellido paterno: ");
-    String apellidoM = entradaTexto("Apellido materno: ");
-    long fechaNac = entrada("Fecha de nacimiento (DDMMYYYY): ");
-    scanner.nextLine();
-    long edad = entrada("Edad:");
-    scanner.nextLine();
-    String sexo = entradaTexto("Sexo (H/M/otro): ");
-    String correoE = entradaTexto("Correo electrónico: ");
-    long telefonoC = entrada("Teléfono: ");
-
-    try(FileWriter writer = new FileWriter("clientes.csv", true)) {
-      writer.append(idCliente + "," + nombreC + "," + apellidoP + "," + apellidoM + "," + fechaNac + "," + edad + "," + sexo + "," + correoE + "," + telefonoC + "\n");
-      System.out.println("Cliente agregado exitosamente.");
-    } catch (IOException e) {
-      System.out.println("Error al escribir en el archivo CSV: " + e.getMessage());
+      //Pedir los datos del premio al usuario y los guardar en variables
+      long idPremio = entrada("ID del Premio (debe ser un número): ");
+      scanner.nextLine();
+      String nombreP = entradaTexto("Nombre del Premio: ");
+      String categoria = entradaTexto("Categoría del Premio: (Bajo, Medio o Alto): ");
+      String rangoEdad = entradaTexto("Rango de Edad (infantil 3-12, juvenil 13-17, adulto 18+): ");
+      long puntos = entrada("Puntos necesarios para canjear el premio: ");
+      scanner.nextLine();
+      long cantidadS = entrada("Cantidad de premios en sucursal: ");
+      scanner.nextLine();
+      long idSucursal = entrada("ID de la Sucursal donde se encuentra el premio: ");
+    
+      // Escribir los datos en el archivo CSV
+      try(FileWriter writer = new FileWriter("premios.csv", true)) {
+        writer.append(idPremio + "," + nombreP + "," + categoria + "," + rangoEdad + "," + puntos + "," + cantidadS + "," + idSucursal + "\n");
+        System.out.println("Premio agregado exitosamente.");
+      } catch (IOException e) {
+        System.out.println("Error al escribir en el archivo CSV: " + e.getMessage());
+      } 
+      //Manejo de excepciones
+    } catch (Exception e) {
+      System.out.println("Error al agregar el premio: " + e.getMessage());
     }
-  } catch (Exception e) {
-    System.out.println("Error al agregar el cliente: " + e.getMessage());
   }
-}
 
-/**
- * Método auxiliar para leer una entrada de texto.
- * @param mensaje Mensaje para el usuario.
- * @return Entrada de texto válida.
- */
-private static String entradaTexto(String mensaje) {
-  System.out.print(mensaje);
-  return scanner.nextLine();
+  /**
+   * Método para agregar clientes
+   */
+  public static void agregarCliente(){
+    try{
+      System.out.println("Ingresa los siguientes datos para el cliente:");
 
-  }    
+      //Pedir los datos del cliente al usuario y los guardar en variables
+      long idCliente = entrada("ID del cliente (debe ser un número): ");
+      scanner.nextLine();
+      String nombreC = entradaTexto("Nombre del cliente: ");
+      String apellidoP = entradaTexto("Apellido paterno: ");
+      String apellidoM = entradaTexto("Apellido materno: ");
+      long fechaNac = entrada("Fecha de nacimiento (DDMMYYYY): ");
+      scanner.nextLine();
+      long edad = entrada("Edad:");
+      scanner.nextLine();
+      String sexo = entradaTexto("Sexo (H/M/otro): ");
+      String correoE = entradaTexto("Correo electrónico: ");
+      long telefonoC = entrada("Teléfono: ");
 
+      // Escribir los datos en el archivo CSV
+      try(FileWriter writer = new FileWriter("clientes.csv", true)) {
+        writer.append(idCliente + "," + nombreC + "," + apellidoP + "," + apellidoM + "," + fechaNac + "," + edad + "," + sexo + "," + correoE + "," + telefonoC + "\n");
+        System.out.println("Cliente agregado exitosamente.");
+      } catch (IOException e) {
+        System.out.println("Error al escribir en el archivo CSV: " + e.getMessage());
+      }
+      //Manejo de excepciones
+    } catch (Exception e) {
+      System.out.println("Error al agregar el cliente: " + e.getMessage());
+    }
+  }
 }
