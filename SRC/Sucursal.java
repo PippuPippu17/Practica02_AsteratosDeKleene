22314493c40d2c3b1acf27040585576ebc91d5c5
MicaelNF <micaelnolascoflores@gmail.com>
2026-09-14 04:08:48 -0600

    Agregó los métodos necesarios para la clase Sucursal.

--- SRC/Sucursal.java
+++ SRC/Sucursal.java
@@ -2,17 +2,185 @@
 import exceptions.HorarioException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Sucursal {
-    private int idSucursal = 0;
-    private String nombre = "";
-    private String calle = "";
-    private int numExterior = 0;
-    private String numeroInterior = "";
-    private String colonia = "";
-    private String estado = "";
-    private long telefono = 0;
-    private Horario horario = null; // Formato "Lun-Vie 11:00-21:00|Sab-Dom 10:00-22:00"
+    private int idSucursal;
+    private String nombre;
+    private String calle;
+    private int numExterior;
+    private String numeroInterior;
+    private String colonia;
+    private String estado;
+    private long telefono;
+    private Horario horario;
+
+    // Constructor por omisión
+    public Sucursal() {
+        this.idSucursal = 0;
+        this.nombre = "";
+        this.calle = "";
+        this.numExterior = 0;
+        this.numeroInterior = "";
+        this.colonia = "";
+        this.estado = "";
+        this.telefono = 0L;
+        this.horario = null;
+    }
+
+    // Constructor con parámetros
+    public Sucursal(int idSucursal, String nombre, String calle, int numExterior,
+            String numeroInterior, String colonia, String estado,
+            long telefono, String horario) throws HorarioException {
+        this.idSucursal = idSucursal;
+        this.nombre = nombre;
+        this.calle = calle;
+        this.numExterior = numExterior;
+        this.numeroInterior = ((numeroInterior == null || numeroInterior.trim().isEmpty()) ? "S/N" : numeroInterior);
+        this.colonia = colonia;
+        this.estado = estado;
+        this.telefono = telefono;
+        this.horario = new Horario(horario);
+    }
+
+    public int getIdSucursal() {
+        return idSucursal;
+    }
+
+    public void setIdSucursal(int idSucursal) {
+        this.idSucursal = idSucursal;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public int getNumExterior() {
+        return numExterior;
+    }
+
+    public void setNumExterior(int numExterior) {
+        this.numExterior = numExterior;
+    }
+
+    public String getNumeroInterior() {
+        return numeroInterior;
+    }
+
+    public void setNumeroInterior(String numeroInterior) {
+        this.numeroInterior = (numeroInterior == null || numeroInterior.trim().isEmpty()) ? "S/N" : numeroInterior;
+    }
+
+    public String getColonia() {
+        return colonia;
+    }
+
+    public void setColonia(String colonia) {
+        this.colonia = colonia;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public long getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(long telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getHorario() {
+        return horario.toString();
+    }
+
+    public void setHorario(String horario) throws HorarioException {
+        this.horario = new Horario(horario);
+    }
+
+    public String toCSV() {
+        String strHorario = ((this.horario != null) ? this.horario.toString() : "");
+        return this.idSucursal + "," +
+                this.nombre + "," +
+                this.calle + "," +
+                this.numExterior + "," +
+                this.numeroInterior + "," +
+                this.colonia + "," +
+                this.estado + "," +
+                this.telefono + "," +
+                strHorario;
+    }
+
+    /**
+     * Método para reconstruir una instancia de Sucursal a partir de una línea del
+     * CSV.
+     */
+    public static Sucursal fromCSV(String lineaCSV) throws HorarioException, NumberFormatException {
+        String[] datos = lineaCSV.split(",");
+        if (datos.length < 9) {
+            throw new IllegalArgumentException("Error: la línea CSV no tiene todas las columnas que necestiamos.");
+        }
+
+        int id = Integer.parseInt(datos[0].trim());
+        String nombre = datos[1].trim();
+        String calle = datos[2].trim();
+        int numExt = Integer.parseInt(datos[3].trim());
+        String numInter = datos[4].trim();
+        String colonia = datos[5].trim();
+        String estado = datos[6].trim();
+        long telefono = Long.parseLong(datos[7].trim());
+        String horario = datos[8].trim();
+
+        return new Sucursal(id, nombre, calle, numExt, numInter, colonia, estado, telefono, horario);
+    }
+
+    @Override
+    public String toString() {
+        return "----------------------------------------Sucursal----------------------------------------\n" +
+                "ID Sucursal\t: " + idSucursal + "\n" +
+                "Nombre\t \t: " + nombre + "\n" +
+                "Dirección\t: " + calle + ", Num.Ext." + numExterior +
+                ", Num.Int." + numeroInterior + ", Col." + colonia + ", " + estado + "\n" +
+                "Teléfono\t: " + telefono + "\n" +
+                "Horario\t \t: " + horario.toString() + "\n" +
+                "----------------------------------------------------------------------------------------";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Sucursal sucursal = (Sucursal) o;
+        return idSucursal == sucursal.idSucursal;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idSucursal);
+    }
 }
 
 class Horario {
@@ -283,7 +451,8 @@ class Horario {
 class Prueba {
     public static void main(String[] args) {
         try {
-            Horario prueba = new Horario("Lun-Mar 12:00-14:00|Mie-Vie 00:01-00:02");
+            Sucursal prueba = new Sucursal(1, "Sucursal Insana", "Av. Delfin Madrigal", 112, "A6304", "Santo Domingo",
+                    "CDMX", 5548665420L, "Lun-Vie 11:00-21:00|Sab-Dom 10:00-22:00");
             System.out.println(prueba);
         } catch (Exception e) {
             System.out.println(e);
